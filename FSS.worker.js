c23f0534aa2bf58a230afebd99bb49deef5ed7fc
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-07-03 00:04:07 -0700

    Updates

--- FSS.worker.js
+++ FSS.worker.js
@@ -1,14 +1,18 @@
 // eslint-disable-line no-restricted-globals
-function FibonacciSunflowerSpiral(radius, width, height, n, pixels) {
+function FibonacciSunflowerSpiral(radius, width, height, fillPercent, pixels) {
     let points = []
 
     const x0 = width/2
     const y0 = height/2
     const angle = Math.PI * (3 - Math.sqrt(5))
+    const max_r = Math.sqrt(((width**2)/4)+((height**2)/4))*(fillPercent/100)
+    let r = 0
+    let i = 0
 
-    for (let i = 0; i < n; i++){
-        let r = 5 * Math.sqrt(i)
+    while (r < max_r) {
+        r = 5 * Math.sqrt(i)
         let theta = i * angle
+        i++
         let x = x0 + r * Math.cos(theta)
         let y = y0 + r * Math.sin(theta)
         if (x < 0 || x >= width || y < 0 || y >= height)
@@ -21,7 +25,7 @@ function FibonacciSunflowerSpiral(radius, width, height, n, pixels) {
 }
 
 self.addEventListener('message', function(event) {
-    const {radius, width, height, n, pixels} = event.data;
-    const points = FibonacciSunflowerSpiral(radius, width, height, n, pixels)
+    const {radius, width, height, fillPercent, pixels} = event.data;
+    const points = FibonacciSunflowerSpiral(radius, width, height, fillPercent, pixels)
     self.postMessage(points)
 })
